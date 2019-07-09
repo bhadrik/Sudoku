fec5d37b540e18370c3916e19930f348cb5ea9d0
Bhadrik Jagad <jagadbhadrik4@gmail.com>
2019-07-09 23:16:10 +0530

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
+class What_to_fill;
 void read();
 void display();
-int count();
-class What_to_fill;
-void fill(What_to_fill obj);
+int count_blank();
+void fill(What_to_fill,short);
 short int arr[9][9]={1,0,0,0,5,6,0,0,0, //1
 					 0,0,9,0,0,0,4,0,0, //2
 					 0,0,0,7,0,9,0,0,0, //3
@@ -14,6 +14,7 @@ short int arr[9][9]={1,0,0,0,5,6,0,0,0, //1
 					 2,0,0,5,0,0,8,0,0, //7
 					 0,9,7,0,3,0,0,0,0, //8
 					 0,6,4,0,0,0,0,0,1 }; //9
+
 class What_to_fill{
 public:
 	short add_i,add_j;
@@ -21,57 +22,59 @@ public:
 };
 
 int main(){
-	short blank;
+	short	blank;
 	//read();
 	display();
-	blank = count();
+	blank = count_blank();
 
 	What_to_fill obj[blank];
 
-	short k=0;
+	short k=0,l=0,a=0,b=0;
 
 	for(short i=0;i<9;i++){
 		for(short j=0;j<9;j++){
 			if(arr[i][j]==0){
-					obj[k].add_i=i;
-					obj[k].add_j=j;
-					k++;
+				obj[k].add_i=i;
+				obj[k].add_j=j;
+				fill(obj[k],k);
+				k++;
 			}
 		}
 	}
-
-	fill(obj[0]);
 
 	cin.get();
 	return 0;
 }
 
-void fill(What_to_fill obj){
+void fill(What_to_fill obj,short k){
 	short a=0,b=0,l=0;
-//1 row
+
 	for(a=0;a<9;a++){
 		if(arr[obj.add_i][a]!=0){ obj.arr[l] = arr[obj.add_i][a]; l++; }
 	}
 
-/*2 Column*/
 	for(a=0;a<9;a++){
 		if(arr[a][obj.add_j]!=0){ obj.arr[l] = arr[a][obj.add_j]; l++; }
 	}
 
-/*3 3x3 Box*/
 	if(obj.add_i<3) a=0; if(obj.add_i<6) a=3; if(obj.add_i<9) a=6;
 	if(obj.add_j<3) b=0; if(obj.add_j<6) b=3; if(obj.add_j<9) b=6;
-	for(a;a<(a+3);a++){
-		for(b;b<(b+3);b++){
-			if(arr[a][b]!=0){ obj.arr[l] = arr[obj.add_i][a]; l++; }
+
+	short x=a+3,y=b+3;
+
+	for(a;a<x;a++){
+		for(b;b<y;b++){
+			if(arr[a][b]!=0){ obj.arr[l] = arr[a][b]; l++; }
 		}
 	}
-	
-	cout<<"\nOBJ 0 detail\n";
+
+	cout<<"\nOBJ "<<k<<" detail\n";
 	cout<<"add_i:"<<obj.add_i<<endl;
 	cout<<"add_j:"<<obj.add_j<<endl;
-	for(int a=0;a<9;a++)
-	cout<<"arr "<<a<<":"<<obj.arr<<endl;
+	cout<<"1 2 3 4 5 6 7 8 9"<<endl;
+	for(int a=0;a<9;a++)	cout<<obj.arr[a]<<" ";
+	cout<<"\n";
+
 }
 
 void read(){
@@ -87,7 +90,7 @@ void read(){
 	}
 }
 
-int count(){
+int count_blank(){
 	int count=0;
 	for(int i=0;i<9;i++){
 		for(int j=0;j<9;j++){
