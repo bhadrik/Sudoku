f44e154d8f989dc58f96dfe0af290d9c7eec3d9e
Bhadrik Jagad <jagadbhadrik4@gmail.com>
2020-09-17 14:42:16 +0530

    Update Sudoku.h

--- Sudoku.h
+++ Sudoku.h
@@ -15,7 +15,7 @@ public:
 		//Setup all objects
 		for(short i=0;i<9;i++){
 			for(short j=0;j<9;j++){
-                obj[i][j].set(0,3,0);
+                		obj[i][j].set(0,3,0);
 				if(sudoku[i][j]==0){
 					obj[i][j].set(0,1,i);	//rowAddress
 					obj[i][j].set(0,2,j);	//columnAddress
@@ -23,12 +23,12 @@ public:
 					k++;
 				}
 				else{
-                    obj[i][j].changStatus(1);
-                    obj[i][j].set(0,3,0);     // objectNumber of already filled number is 0.
+					obj[i][j].changStatus(1);
+					obj[i][j].set(0,3,0);     // objectNumber of already filled number is 0.
 				}
 			}
 		}
-
+		
 		bool again = true;
 		bool againFix = true;
 		short limit = 0;
@@ -39,7 +39,7 @@ public:
 			short temp = totalEmpty;
 
 			for(short i=0;i<9;i++)
-            for(short j=0;j<9;j++){
+            		for(short j=0;j<9;j++){
 				if(!obj[i][j].isFilled()){
 					processObj(obj[i][j]);
 				}
@@ -49,44 +49,43 @@ public:
 			
 
 			if(temp == totalEmpty){
-                if(again){
-                    loopBackup = loop-2;
-                    copy(&sudoku[0][0], &sudoku[0][0]+81, &suBackup[0][0]);
-                    copy(&obj[0][0], &obj[0][0]+81, &objBackup[0][0]);
-                    athiyo(m,n,value);
-                    again = false;
-                }
-                else if(againFix){
-                    loop = loopBackup;
-                    copy(&suBackup[0][0], &suBackup[0][0]+81, &sudoku[0][0]);
-                    copy(&objBackup[0][0], &objBackup[0][0]+81, &obj[0][0]);
-                    fix(m,n,value);
-                    againFix = false;
-                }
+                		if(again){
+					loopBackup = loop-2;
+					copy(&sudoku[0][0], &sudoku[0][0]+81, &suBackup[0][0]);
+					copy(&obj[0][0], &obj[0][0]+81, &objBackup[0][0]);
+					athiyo(m,n,value);
+					again = false;
+				}
+				else if(againFix){
+					loop = loopBackup;
+					copy(&suBackup[0][0], &suBackup[0][0]+81, &sudoku[0][0]);
+					copy(&objBackup[0][0], &objBackup[0][0]+81, &obj[0][0]);
+					fix(m,n,value);
+					againFix = false;
+				}
 				if (limit == 4) {
 					break;
 				}
-                limit++;
-            }
+				limit++;
+			}
 			loop++;
 		}
 
 		if(loop >= MAXLOOP || totalEmpty){
-            cout<<"+--------------------------------------------+\n"
+			cout<<"+--------------------------------------------+\n"
 			    <<"| CAN'T SOLVE!                               |\n"
 			    <<"| Your sudoku is harder than hard :)         |\n"
 			    <<"+--------------------------------------------+\n";
-            cout<<"totalEmpty: "<<totalEmpty<<endl;
+			cout<<"totalEmpty: "<<totalEmpty<<endl;
 		}
-
 	}
 
 	void input(){
 		bool input = false;
-
+		
 		cout<<"+-----------------+ +--------------------+\n"
-			<<"| 1->INPUT SUDOKU | | 0->GO WITH DEFAULT |\n"
-			<<"+-----------------+ +--------------------+\n";
+		    <<"| 1->INPUT SUDOKU | | 0->GO WITH DEFAULT |\n"
+		    <<"+-----------------+ +--------------------+\n";
 
 		cin>>input;
 		if(!input){
@@ -101,13 +100,15 @@ public:
 
 		char ch='A';
 		cout<<"  1 2 3 4 5 6 7 8 9\n"
-				<<"--------------------\n";
+		    <<"--------------------\n";
+		
 		for(short i=0;i<9;i++){
 			cout<<ch++<<'|';
 			for(short j=0;j<9;j++){
 				cin>>sudoku[i][j];
 			}
 		}
+		
 		cout<<endl;
 		blank = countEmptyFields();
 		totalEmpty = blank;
@@ -115,9 +116,9 @@ public:
 
 	void display(){
 		cout<<"    1 2 3 | 4 5 6 | 7 8 9\n"
-				<<"  +-------+-------+-------+\n";
+		    <<"  +-------+-------+-------+\n";
 		for(short i=0;i<9;i++){
-				cout<<i+1;
+			cout<<i+1;
 			for(short j=0;j<9;j++){
 				if(j==0) cout<<' ';
 				if(j%3==0) cout<<"| ";
@@ -134,8 +135,8 @@ public:
 	void displayObj(short index){
 		for(short i=0;i<9;i++)
 		for(short j=0;j<9;j++)
-            if(obj[i][j].get(0,3) == index)
-                obj[i][j].display();
+			if(obj[i][j].get(0,3) == index)
+				obj[i][j].display();
 	}
 
 	//display object by providing its location in sudoku
@@ -149,14 +150,13 @@ private:
 
 	BlankSpace obj[9][9];
 	BlankSpace objBackup[9][9];
-
-    short loop = 0;
-    short loopBackup = 0;
-
-    short blank = countEmptyFields();
-    short totalEmpty;
-
-    short m=-1,n=-1,value=0;
+	
+	short loop = 0;
+	short loopBackup = 0;
+	
+	short blank = countEmptyFields();
+	short totalEmpty;
+	short m=-1,n=-1,value=0;
 
 	//fill function fills the mask field of sudoku
 	void processObj(BlankSpace &objOne){
@@ -172,36 +172,34 @@ private:
 
 		for(short i=a;i<x;i++)
 		for(short j=b;j<y;j++){
-            objOne.set(1,sudoku[i][j],0);
+			objOne.set(1,sudoku[i][j],0);
 		}
 
-		//One step ahed search
+		//One step ahead search
 		objOne.finalProcess(sudoku);
 
 		deepSearch(objOne, a, b);
 	}
 
 	void deepSearch(BlankSpace &objOne, short a, short b){
-	    short x=a+3, y=b+3;
-        if(loop>0){
-            for(int l=1;l<10;l++){
-
-                if(objOne.get(1,l)==1){
-                    bool possible = true;
-                    for(short i=a;i<x;i++){
-                    for(short j=b;j<y;j++){
-                        //    objectNumber != 0      Not same object
-                        if(obj[i][j].get(0,3) !=0 && obj[i][j].get(0,3) != objOne.get(0,3) && !obj[i][j].isFilled()){
+		short x=a+3, y=b+3;
+		if(loop>0){
+			for(int l=1;l<10;l++)
+			if(objOne.get(1,l)==1){
+				bool possible = true;
+				for(short i=a;i<x;i++){
+					for(short j=b;j<y;j++){
+						//    objectNumber != 0      Not same object
+						if(obj[i][j].get(0,3) !=0 && obj[i][j].get(0,3) != objOne.get(0,3) && !obj[i][j].isFilled()){
 							obj[i][j].manualProcess(sudoku);
-                            if(obj[i][j].get(1,l) !=0){ possible = false; break; }
-                        }
-                    }
-                        if(!possible) break;
-                    }
-					if (possible) { objOne.forceApply(sudoku, l); break; }
-                }
-            }
-        }
+							if(obj[i][j].get(1,l) !=0){ possible = false; break; }
+						}
+					}
+					if(!possible) break;
+				}
+				if (possible) { objOne.forceApply(sudoku, l); break; }
+			}
+		}
 	}
 
 	//this function fill one of two blank space first found 3x3 box
@@ -209,7 +207,7 @@ private:
 		/*
 		* Oho! there is "minimum=2", whichi is the only working number 
 		* if this number go further bigger then 2 then fix function could not 
-		* fix that, untill update ;)
+		* fix that, untill next update ;)
 		*/
 
 		for (short minimum = 2 ; minimum < 9; minimum++) {
@@ -223,15 +221,14 @@ private:
 					if (!obj[i][j].isFilled()) { emptyObj++; p = i; q = j; }
 					if (emptyObj > minimum) break;
 				}                                 
-				if (emptyObj == minimum) {
-					for (short i = 1; i < 10; i++)
-					if (obj[p][q].get(1, i)) {
-						if(minimum>2) cout << "Minimum:" << minimum << " this may not work! " << endl;
-						//cout << "A(" << p+1 << "," << q+!1 << ")" << endl;
-						obj[p][q].forceApply(sudoku, i);
-						value = i;
-						return;
-					}
+				if (emptyObj == minimum)
+				for (short i = 1; i < 10; i++)
+				if (obj[p][q].get(1, i)) {
+					if(minimum>2) cout << "Minimum:" << minimum << " this may not work! " << endl;
+					//cout << "A(" << p+1 << "," << q+!1 << ")" << endl;
+					obj[p][q].forceApply(sudoku, i);
+					value = i;
+					return;
 				}
 			}
 			minimum++;
@@ -242,18 +239,18 @@ private:
 	void fix(short m, short n, short value){
 		obj[m][n].manualProcess(sudoku);
 		for(int i=1;i<10;i++){
-            if(i!=value && obj[m][n].get(1,i)==1){
+			if(i!=value && obj[m][n].get(1,i)==1){
 				//cout << "F(" << m+1 << "," << n+1 << ")" << endl;
-                obj[m][n].forceApply(sudoku,i);
-                break;
-            }
+				obj[m][n].forceApply(sudoku,i);
+				break;
+			}
 		}
 	}
 
 	short countBlankObjects(){
 		short tmp=0;
 		for(short i=0;i<9;i++)
-        for(short j=0;j<9;j++){
+		for(short j=0;j<9;j++){
 			if(!obj[i][j].isFilled()) tmp++;
 		}
 		return tmp;
